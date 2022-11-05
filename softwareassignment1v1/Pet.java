baa55b01f2e08180e9c629c008579140266459fe
malcolmrmoore18 <117340542+malcolmrmoore18@users.noreply.github.com>
2022-11-04 22:11:20 -0500

    Add files via upload
    Version 1 of the Assignment 1 Project

---
+++ softwareassignment1v1/Pet.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package softwareassignment1v1;
+
+/**
+ *
+ * @author malxm
+ */
+public class Pet {
+    private int ID;
+    private String name;
+    private int age;
+    
+    public Pet(int ID, String name, int age){
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+}
